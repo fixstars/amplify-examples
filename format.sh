589dcb48b07e62795700a75b95152671883894f0
Yoshiki Matsuda <y_matsuda@fixstars.com>
2023-05-22 01:48:00 +0900

    👷 Add format stage in CI

--- format.sh
+++ format.sh
@@ -3,4 +3,4 @@ shopt -s globstar
 
 for f in $(/bin/ls -1 **/*.ipynb); do /bin/cat <<< $(jq --indent 1 --monochrome-output '(.cells[] | select(has("outputs")) | .outputs) = []  | (.cells[] | select(has("execution_count")) | .execution_count) = null  | .metadata = {"language_info": {"name": "python", "pygments_lexer": "ipython3"}} | .cells[].metadata = {}' $f) > $f; done
 
-nbqa black **/*.ipynb --nbqa-mutate
+black **/*.ipynb **/*.py
