c14bb46ff6fe542a9e8addef4e54079f39502589
Yuki Minamoto <yuki.minamoto@fixstars.com>
2023-05-23 12:11:01 +0900

    ✨ Add a script to generate html from ipynb

---
+++ make-html.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+for NBPATH in $(shopt -s globstar && /bin/ls -1 notebooks/**/samples/*.ipynb); do
+    JP_OR_EN=$(echo ${NBPATH} | cut -c 11-12)
+    DIR=./html/${JP_OR_EN}/
+    OUTPUT=$(jupyter nbconvert --embed-images --output-dir=$DIR --to html "$NBPATH")
+    if [ -n "$OUTPUT" ]; then
+        echo "[NbConvertApp] Converting notebook $NBPATH to html" 1>&2
+        echo "$OUTPUT" 1>&2
+    fi
+done
+
