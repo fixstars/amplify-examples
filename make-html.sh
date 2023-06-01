6863d12f6a3728a46ab7b32ae11d37c0cea0fa38
Yuki Minamoto <yuki.minamoto@fixstars.com>
2023-06-01 18:10:28 +0900

    🚚 Update directory structure (1)

--- make-html.sh
+++ make-html.sh
@@ -1,5 +1,18 @@
 #!/bin/bash -eu
 
+
+# For newer directories
+for NBPATH in $(shopt -s globstar && /bin/ls -1 notebooks/**/examples/*.ipynb notebooks/**/tutorials/*.ipynb); do
+    DIR=html/$(echo ${NBPATH} | cut -d / -f 2-3)/
+    OUTPUT=$(jupyter nbconvert --embed-images --output-dir=$DIR --to html "$NBPATH")
+    if [ -n "$OUTPUT" ]; then
+        echo "[NbConvertApp] Converting notebook $NBPATH to html" 1>&2
+        echo "$OUTPUT" 1>&2
+    fi
+done
+
+
+# For older directories
 for NBPATH in $(shopt -s globstar && /bin/ls -1 notebooks/**/samples/*.ipynb); do
     JP_OR_EN=$(echo ${NBPATH} | cut -c 11-12)
     DIR=./html/${JP_OR_EN}/
@@ -9,4 +22,5 @@ for NBPATH in $(shopt -s globstar && /bin/ls -1 notebooks/**/samples/*.ipynb); d
         echo "$OUTPUT" 1>&2
     fi
 done
+
 
